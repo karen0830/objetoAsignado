3a3e9807a349833034d68e33c195f7cc17cdd4ec
karen lópez <lopezkaren43567@gmail.com>
2023-06-07 15:16:47 -0500

    first commit

---
+++ Perro/src/Principal.java
@@ -0,0 +1,10 @@
+import vista.procesos.*;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Procesos misP = new Procesos();
+		misP.inicio();
+	}
+
+}
